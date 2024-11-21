0384f4670075de02f7f57a91c717c36d27c55abf
Sauren Khosla <sksauren@gmail.com>
2024-11-20 23:44:00 -0800

    remove debugging comments

--- app/animated-icons/social/spotify.tsx
+++ app/animated-icons/social/spotify.tsx
@@ -10,7 +10,6 @@ const SpotifyIcon = () => {
   const logoControls = useAnimation();
 
   const animateLogo = async () => {
-    // Bottom arc animation
     await Promise.all([
       bottomArcControls.start({
         stroke: "#1ed760",
@@ -22,7 +21,6 @@ const SpotifyIcon = () => {
       transition: { duration: 0.15 },
     });
 
-    // Top arc animation
     await Promise.all([
       topArcControls.start({
         stroke: "#1ed760",
@@ -34,7 +32,6 @@ const SpotifyIcon = () => {
       transition: { duration: 0.15 },
     });
 
-    // Middle arc animation
     await Promise.all([
       middleArcControls.start({
         stroke: "#1ed760",
@@ -46,7 +43,6 @@ const SpotifyIcon = () => {
       transition: { duration: 0.15 },
     });
 
-    // Top arc animation again
     await Promise.all([
       topArcControls.start({
         stroke: "#1ed760",
@@ -58,7 +54,6 @@ const SpotifyIcon = () => {
       transition: { duration: 0.15 },
     });
 
-    // Bottom arc animation again
     await Promise.all([
       bottomArcControls.start({
         stroke: "#1ed760",
@@ -70,7 +65,6 @@ const SpotifyIcon = () => {
       transition: { duration: 0.15 },
     });
 
-    // Additional middle arc animation
     await Promise.all([
       middleArcControls.start({
         stroke: "#1ed760",
@@ -82,7 +76,6 @@ const SpotifyIcon = () => {
       transition: { duration: 0.15 },
     });
 
-    // Additional top arc animation
     await Promise.all([
       topArcControls.start({
         stroke: "#1ed760",
@@ -94,7 +87,6 @@ const SpotifyIcon = () => {
       transition: { duration: 0.15 },
     });
 
-    // Final pulsate with additional scale animation
     await logoControls.start({
       scale: [1, 1.1, 1],
       transition: { duration: 0.7 },
@@ -102,7 +94,6 @@ const SpotifyIcon = () => {
   };
 
   const resetLogo = () => {
-    // Reset arcs to white and scale to 1
     bottomArcControls.start({ stroke: "#fff" });
     middleArcControls.start({ stroke: "#fff" });
     topArcControls.start({ stroke: "#fff" });
@@ -122,9 +113,7 @@ const SpotifyIcon = () => {
         xmlns="http://www.w3.org/2000/svg"
         animate={logoControls}
       >
-        {/* Circle background */}
         <circle cx="16" cy="16" r="16" fill="#1ed760" />
-        {/* Top arc */}
         <motion.path
           id="top-arc"
           d="M6.5 11.5c6-2 13-2 19.5 2.5"
@@ -134,7 +123,6 @@ const SpotifyIcon = () => {
           strokeLinecap="round"
           animate={topArcControls}
         />
-        {/* Middle arc */}
         <motion.path
           id="middle-arc"
           d="M7.5 17c4-2 12-1.25 16 2"
@@ -144,7 +132,6 @@ const SpotifyIcon = () => {
           strokeLinecap="round"
           animate={middleArcControls}
         />
-        {/* Bottom arc */}
         <motion.path
           id="bottom-arc"
           d="M9 22c3.5-1.5 8.5-1 12 1.5"
